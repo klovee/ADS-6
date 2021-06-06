ee5b2bb68e0c0063780d472f6634d847c44530b2
klovee <79109849+klovee@users.noreply.github.com>
2021-06-06 19:43:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,25 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+BST<std::string>Tree;
+  char difFromAToa = 'a' - 'A';
+  std::string word = "";
+  std::ifstream file(filename);
+  if (!file) {
+    std::cout << "Error reading file" << std::endl;
+    return Tree;
+  }
+  while (!file.eof()) {
+    char words = file.get();
+    if ((words >= 'A' && words <= 'Z') || (words >= 'a' && words <= 'z')) {
+      if (words >= 'A' && words <= 'Z')
+        words += difFromAToa;
+      wcount += words;
+    } else if (wcount != "") {
+      Tree.add(wcount);
+      wcount = "";
+    }
+  }
+  file.close();
+  return Tree;
 }
